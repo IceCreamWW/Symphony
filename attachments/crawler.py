1dd1e1debd23e64e4ded413823c7e30d3bbc6ef3
Yuhong Zhong <nanua.zqz@gmail.com>
2017-12-10 10:02:37 +0800

    modified data loader

--- attachments/crawler.py
+++ attachments/crawler.py
@@ -20,7 +20,7 @@ video_dict = dict()
 agent = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
 headers = {"User-Agent": agent}
 
-for movie_name in movie_name_list:
+for index, movie_name in enumerate(movie_name_list):
     search_url = search_url_generator(movie_name)
     search_respond = requests.get(search_url)
     search_respond.encoding = search_respond.apparent_encoding
@@ -33,12 +33,22 @@ for movie_name in movie_name_list:
     content_html_str = content_respond.content
     content_soup = BeautifulSoup(content_html_str, "lxml")
     description = str(content_soup.find("div", class_="summary_text").next_element).strip()
-    if not content_soup.find("a", class_="slate_button") is None:
-        video = "http://www.imdb.com" + content_soup.find("a", class_="slate_button").get("href")
+    poster_div = content_soup.find("div", class_="poster")
+    if not poster_div is None:
+        video_src = poster_div.a.img.get("src")
+        video_type = video_src[video_src.rfind("."):]
+        video_respond = requests.get(video_src)
+        video_bytes = video_respond.content
+        video_file = open("../main_page/static/movie.video/{0}{1}".format(urllib.parse.quote_plus(movie_name),
+                                                                          video_type), "wb")
+        video_file.write(video_bytes)
+        video_file.close()
+        video = "movie.video/{0}{1}".format(urllib.parse.quote_plus(movie_name), video_type)
     else:
         video = ""
     description_dict[movie_name] = description
     video_dict[movie_name] = video
+    print("Progress:{}".format((index + 1) / len(movie_name_list)))
 
 description_file = open("./movie_description.bin", "wb")
 video_file = open("./movie_video.bin", "wb")
